911bc4cbe6c44102470a3c9d8e80b7ac62e446a9
mjenks <rikku.hotaru@gmail.com>
2022-04-30 14:52:55 -0400

    day 1
    Set up code and parse input

---
+++ day1.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 30 14:44:49 2022
+
+@author: mjenks
+"""
+
+def parse(puzzle_input):
+    data = []
+    for line in puzzle_input:
+        line = list(line.strip())
+        sign = line[0]
+        value = int(''.join(line[1:]))
+        if sign == '+':
+            data.append(value)
+        else:
+            data.append(-1*value)
+    return data
+    
+def solve(puzzle_data):
+    return 0, 0
+
+puzzle_path = "input_day1.txt"
+with open(puzzle_path) as f:
+    puzzle_input = f.readlines()
+    
+puzzle_data = parse(puzzle_input)
+solution1, solution2 = solve(puzzle_data)
+
+print(solution1)
+print(solution2)
