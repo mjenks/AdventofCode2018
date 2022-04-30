16e2317ea09699632fac565b26de17f5054fe0c6
mjenks <rikku.hotaru@gmail.com>
2022-04-30 15:17:57 -0400

    part 1
    use counter from collections to check the number of times each letter is
    in each id

--- day2.py
+++ day2.py
@@ -5,6 +5,8 @@ Created on Sat Apr 30 15:03:35 2022
 @author: mjenks
 """
 
+from collections import Counter
+
 def parse(puzzle_input):
     data = []
     for line in puzzle_input:
@@ -13,7 +15,15 @@ def parse(puzzle_input):
     return data
     
 def solve(puzzle_data):
-    return 0, 0
+    double = 0
+    triple = 0
+    for box in puzzle_data:
+        counts = Counter(box)
+        if 2 in counts.values():
+            double += 1
+        if 3 in counts.values():
+            triple += 1
+    return double*triple, 0
 
 puzzle_path = "input_day2.txt"
 with open(puzzle_path) as f:
