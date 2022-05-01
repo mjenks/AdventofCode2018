3d1876b96b79da35e342fa8aadb30b2ab6e7ab84
mjenks <rikku.hotaru@gmail.com>
2022-05-01 12:37:48 -0400

    part 1
    uses a zero's matrix for the cloth and adds to it for each claim

--- day3.py
+++ day3.py
@@ -5,21 +5,29 @@ Created on Sat Apr 30 15:29:13 2022
 @author: mjenks
 """
 
+import numpy as np
+
 def parse(puzzle_input):
     data = []
     for line in puzzle_input:
         line = line.strip()
         elf = line.split()[0]
         line = line.split(': ')
-        y = int(line[0].split(',')[0].split()[-1])
-        x = int(line[0].split(',')[1])
+        x = int(line[0].split(',')[0].split()[-1])
+        y = int(line[0].split(',')[1])
         w = int(line[1].split('x')[0])
-        h = int(line[1].split('x')[0])
-        data.append([elf,y,x,w,h])
+        h = int(line[1].split('x')[1])
+        data.append([elf,x,y,w,h])
     return data
     
 def solve(puzzle_data):
-    return 0, 0
+    size = 1000
+    cloth = np.zeros((size,size))
+    for elf in puzzle_data:
+        ident, x, y, w, h = elf
+        cloth[x:x+w, y:y+h] += 1
+    overlap = np.size(np.where(cloth >= 2)[0])
+    return overlap, 0
 
 puzzle_path = "input_day3.txt"
 with open(puzzle_path) as f:
@@ -29,4 +37,4 @@ puzzle_data = parse(puzzle_input)
 solution1, solution2 = solve(puzzle_data)
 
 print(solution1)
-print(solution2)
+#print(solution2)
