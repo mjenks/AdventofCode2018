ede4e55ba6cf402e890bf28643d8f6ba06e85aa3
mjenks <rikku.hotaru@gmail.com>
2022-05-01 12:34:46 -0400

    Revert "part 1"
    This reverts commit 5393ce36907b2a45e8660a98136739a096c3078b.

--- day3.py
+++ day3.py
@@ -11,30 +11,15 @@ def parse(puzzle_input):
         line = line.strip()
         elf = line.split()[0]
         line = line.split(': ')
-        x = int(line[0].split(',')[0].split()[-1])
-        y = int(line[0].split(',')[1])
+        y = int(line[0].split(',')[0].split()[-1])
+        x = int(line[0].split(',')[1])
         w = int(line[1].split('x')[0])
         h = int(line[1].split('x')[0])
-        data.append([elf,x,y,w,h])
+        data.append([elf,y,x,w,h])
     return data
     
 def solve(puzzle_data):
-    cloth = [['.' for j in range(1100)] for i in range(1100)]
-    overlap = 0
-    for elf in puzzle_data:
-        y = elf[1]
-        x = elf[2]
-        w = elf[3]
-        h = elf[4]
-        for a in range(y,y+h):
-            for b in range(x,x+w):
-                if cloth[a][b] == '.':
-                    cloth[a][b] = elf[0]
-                else:
-                    cloth[a][b] = 'x'
-    for row in cloth:
-        overlap += row.count('x')
-    return overlap, cloth
+    return 0, 0
 
 puzzle_path = "input_day3.txt"
 with open(puzzle_path) as f:
@@ -44,4 +29,4 @@ puzzle_data = parse(puzzle_input)
 solution1, solution2 = solve(puzzle_data)
 
 print(solution1)
-#print(solution2)
+print(solution2)
