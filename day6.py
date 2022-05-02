c930d482995bf5b269adcd95e739f4853dba3a2b
mjenks <rikku.hotaru@gmail.com>
2022-05-02 13:27:31 -0400

    part 1
    make grid and find largest area excluding any area that touches an edge

--- day6.py
+++ day6.py
@@ -11,8 +11,30 @@ def parse(puzzle_input):
         data.append([int(x) for x in line.strip().split(', ')])
     return data
     
+def closest(x,y,points):
+    dist = [abs(x-i) + abs(y-j) for i, j in points]
+    close = min(dist)
+    if dist.count(close) == 1:
+        return dist.index(close)
+    else:
+        return -1
+    
 def solve(puzzle_data):
-    return 0, 0
+    grid = [['.' for i in range(400)] for j in range(400)]
+    for y in range(400):
+        for x in range(400):
+            grid[y][x] = closest(x,y, puzzle_data)
+    inf = [False for i in puzzle_data]
+    edge = set(grid[0]) | set(grid[-1]) | set([a[0] for a in grid]) | set([a[-1] for a in grid])
+    for val in edge:
+        inf[val] = True
+    size = []
+    largest_area = 0
+    for i in range(len(puzzle_data)):
+        size.append(sum([a.count(i) for a in grid]))
+        if not inf[i]:
+            largest_area = max(largest_area, size[i])
+    return largest_area, 0
 
 puzzle_path = "input_day6.txt"
 with open(puzzle_path) as f:
