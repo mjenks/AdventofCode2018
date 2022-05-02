749389a5a663dedc0104b27756a1c3f421403f53
mjenks <rikku.hotaru@gmail.com>
2022-05-02 13:38:15 -0400

    part 2
    added a calculation of total distance and summed points with less than
    10000

--- day6.py
+++ day6.py
@@ -15,15 +15,17 @@ def closest(x,y,points):
     dist = [abs(x-i) + abs(y-j) for i, j in points]
     close = min(dist)
     if dist.count(close) == 1:
-        return dist.index(close)
+        return dist.index(close), sum(dist)
     else:
-        return -1
+        return -1, sum(dist)
+
     
 def solve(puzzle_data):
     grid = [['.' for i in range(400)] for j in range(400)]
+    dist = [[0 for i in range(400)] for j in range(400)]
     for y in range(400):
         for x in range(400):
-            grid[y][x] = closest(x,y, puzzle_data)
+            grid[y][x], dist[y][x] = closest(x,y, puzzle_data)
     inf = [False for i in puzzle_data]
     edge = set(grid[0]) | set(grid[-1]) | set([a[0] for a in grid]) | set([a[-1] for a in grid])
     for val in edge:
@@ -34,7 +36,8 @@ def solve(puzzle_data):
         size.append(sum([a.count(i) for a in grid]))
         if not inf[i]:
             largest_area = max(largest_area, size[i])
-    return largest_area, 0
+    area = sum([sum(a < 10000 for a in b) for b in dist])
+    return largest_area, area
 
 puzzle_path = "input_day6.txt"
 with open(puzzle_path) as f:
