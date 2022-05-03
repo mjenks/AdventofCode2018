e8548df68e3b6e856dc9a262f6b1d4607d48bc08
mjenks <rikku.hotaru@gmail.com>
2022-05-03 18:34:18 -0400

    part 1
    check requirments and do the next alphabetical step

--- day7.py
+++ day7.py
@@ -17,7 +17,23 @@ def parse(puzzle_input):
     return data
     
 def solve(puzzle_data):
-    return 0, 0
+    complete = [False for x in puzzle_data]
+    avail = set()
+    order = []
+    while False in complete:
+        for step in range(len(puzzle_data)):
+            needed = puzzle_data[step]
+            met = True
+            for req in needed:
+                if not complete[string.uppercase.index(req)]:
+                    met = False
+            if met and not complete[step]:
+                avail.add(step)
+        task = min(avail)
+        complete[task] = True
+        avail.remove(task)
+        order.append(task)
+    return ''.join([string.uppercase[step] for step in order]), 0
 
 puzzle_path = "input_day7.txt"
 with open(puzzle_path) as f:
