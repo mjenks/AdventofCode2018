8ea075e643212667d9f8f30d60ba99e385d99b98
mjenks <rikku.hotaru@gmail.com>
2022-05-04 12:04:15 -0400

    part 2
    updated the node class and parser to store child node values and class
    function to calculate the node value

--- day8.py
+++ day8.py
@@ -7,30 +7,45 @@ Created on Wed May 04 11:06:18 2022
 
 class Node:
     def __init__(self, children, meta):
-        self.children = children
+        self.num_children = children
         self.num_meta = meta
         self.meta = []
+        self.children = []
+        
+    def value(self):
+        if self.num_children == 0:
+            return sum(self.meta)
+        else:
+            value = 0
+            for i in self.meta:
+                if i <= self.num_children:
+                    value += self.children[i-1]
+                else:
+                    value += 0
+            return value
 
 def parse(tree):
     children, meta = tree[:2]
     node = Node(children, meta)
     tree = tree[2:]
     for i in range(children):
-        tree = parse(tree)
+        tree, child = parse(tree)
+        node.children.append(child)
     node.meta = tree[:meta]
     puzzle_data.append(node)
-    return tree[meta:]
+    return tree[meta:], node.value()
     
 def solve(puzzle_data):
-    return sum([sum(n.meta) for n in puzzle_data]), 0
+    return sum([sum(n.meta) for n in puzzle_data])
 
 puzzle_path = "input_day8.txt"
 with open(puzzle_path) as f:
     puzzle_input = f.read()
     
 puzzle_data = []
-a = parse([int(x) for x in puzzle_input.strip().split()])
-solution1, solution2 = solve(puzzle_data)
-print len(puzzle_data)
+a,b = parse([int(x) for x in puzzle_input.strip().split()])
+solution1 = solve(puzzle_data)
+solution2 = b
+
 print(solution1)
 print(solution2)
