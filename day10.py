e8f6f9cec342ec163adcd2143df69e519d76cdb9
mjenks <rikku.hotaru@gmail.com>
2022-05-06 16:18:09 -0400

    part 1
    I played with the data to find where it condensed. Then ran the movement
    by time step and printed a display of the points when the height was
    small

--- day10.py
+++ day10.py
@@ -9,6 +9,9 @@ class Point():
     def __init__(self, position, velocity):
         self.x, self.y = position
         self.vx, self.vy = velocity
+    def move(self, step = 1):
+        self.x += self.vx *step
+        self.y += self.vy *step
 
 def parse(puzzle_input):
     data = []
@@ -22,6 +25,25 @@ def parse(puzzle_input):
     return data
     
 def solve(puzzle_data):
+    t = 10080
+    for p in puzzle_data:
+        p.move(10080)
+    while t < 10090:
+        t += 1
+        for p in puzzle_data:
+            p.move(1)
+        x_max = max([a.x for a in puzzle_data])
+        x_min = min([a.x for a in puzzle_data])
+        y_max = max([a.y for a in puzzle_data])
+        y_min = min([a.y for a in puzzle_data])
+        if y_max - y_min < 12:
+            grid = [['.' for i in range(x_max - x_min + 1)] for j in range(y_max - y_min + 1)]
+            for pt in puzzle_data:
+                grid[pt.y-y_min][pt.x-x_min] = '#'
+            print(t)
+            for row in grid:
+                print(''.join(row))
+
     return 0, 0
 
 puzzle_path = "input_day10.txt"
