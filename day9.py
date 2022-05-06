42ee3341aaf423f59358d4796dc454f3980a1f25
mjenks <rikku.hotaru@gmail.com>
2022-05-05 22:37:48 -0400

    part 1
    runs the game for my inputs and returns max score. Also runs tests for
    the examples given

--- day9.py
+++ day9.py
@@ -11,14 +11,47 @@ def parse(puzzle_input):
     
 def solve(puzzle_data):
     num_players, last_marble = puzzle_data
-    return 0, 0
+    circle = [0]
+    current = 0
+    scores = [0 for i in range(num_players)]
+    elf = 0
+    for marble in range(1,last_marble+1):
+        if marble%23 == 0:
+            bonus = (current - 7)%len(circle)
+            scores[elf] += marble + circle[bonus]
+            del circle[bonus]
+            current = bonus%len(circle)
+        else:
+            current = (current + 2)%len(circle)
+            if current == 0:
+                circle.insert(len(circle), marble)
+                current = len(circle) - 1
+            else:
+                circle.insert(current, marble)
+        elf = (elf+1)%num_players
+    
+    return max(scores), scores.index(max(scores))
 
 puzzle_path = "input_day9.txt"
 with open(puzzle_path) as f:
     puzzle_input = f.read()
     
+print "tests:"
+print solve((9,25))[0], 32
+#10 players; last marble is worth 1618 points: high score is 8317
+print solve((10,1618))[0], 8317
+#13 players; last marble is worth 7999 points: high score is 146373
+print solve((13,7999))[0], 146373
+#17 players; last marble is worth 1104 points: high score is 2764
+print solve((17,1104))[0], 2764
+#21 players; last marble is worth 6111 points: high score is 54718
+print solve((21,6111))[0], 54718
+#30 players; last marble is worth 5807 points: high score is 37305  
+print solve((30,5807))[0], 37305
+    
 puzzle_data = parse(puzzle_input)
 solution1, solution2 = solve(puzzle_data)
 
+print "Solutions:"
 print(solution1)
 print(solution2)
