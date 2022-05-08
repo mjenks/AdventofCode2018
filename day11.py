60430f9f2628f1890a8a9f8083871869723900e4
mjenks <rikku.hotaru@gmail.com>
2022-05-07 22:28:23 -0400

    part 2
    I only considered sizes larger than 3 because the max there was larger
    than any 2x2 could be. Then I stopped increasing the size when the max
    was less than 0.

--- day11.py
+++ day11.py
@@ -14,18 +14,30 @@ def power(x, y, num):
         power_level = 0
     return power_level - 5
     
-def solve(puzzle_data):
-    grid = [[power(i, j, puzzle_data) for i in range(1,301)] for j in range(1,301)]
+def max_square(grid, size):
     max_power = 0
-    for y in range(297):
-        for x in range(297):
-            tot_power = sum([sum(a[x:x+3]) for a in grid[y:y+3]])
+    corner = (0,0)
+    for y in range(300-size):
+        for x in range(300-size):
+            tot_power = sum([sum(a[x:x+size]) for a in grid[y:y+size]])
             if tot_power > max_power:
                 max_power = tot_power
                 corner = (x+1, y+1)
-        
-    return corner, 0
-
+    return max_power, corner
+    
+def solve(puzzle_data):
+    grid = [[power(i, j, puzzle_data) for i in range(1,301)] for j in range(1,301)]
+    boxes = []
+    i = 3
+    sqr = max_square(grid, 3) 
+    boxes.append(sqr)
+    while sqr[0] > 0:
+        i += 1
+        sqr = max_square(grid, i)
+        boxes.append(sqr)
+    size = 3 + boxes.index(max(boxes))
+    corner = max(boxes)[1]
+    return boxes[0][1], (corner, size)
     
 puzzle_data = 7803
 solution1, solution2 = solve(puzzle_data)
