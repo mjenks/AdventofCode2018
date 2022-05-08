144e79fc6932639d4cbfebc9b1b27f1cfe6fe4a9
mjenks <rikku.hotaru@gmail.com>
2022-05-07 21:36:07 -0400

    day 11
    set up code and put in value of puzzle input

---
+++ day11.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 07 21:30:32 2022
+
+@author: mjenks
+"""
+
+    
+def solve(puzzle_data):
+    return 0, 0
+
+    
+puzzle_data = 7803
+solution1, solution2 = solve(puzzle_data)
+
+print(solution1)
+print(solution2)
