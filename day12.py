6ab104f3ccb397cab89ca5aded337ec18c7c9c54
mjenks <rikku.hotaru@gmail.com>
2022-05-08 22:26:48 -0400

    day 12
    Set up code and parse input file

---
+++ day12.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 08 22:16:38 2022
+
+@author: mjenks
+"""
+
+def parse(puzzle_input):
+    data = []
+    line = puzzle_input[0]
+    start = line.strip().split(': ')[1]
+    for line in puzzle_input[2:]:
+        rule = line.strip().split(' => ')
+        data.append(rule)
+    return start, data
+    
+def solve(puzzle_data):
+    return 0, 0
+
+puzzle_path = "input_day12.txt"
+with open(puzzle_path) as f:
+    puzzle_input = f.readlines()
+    
+puzzle_data = parse(puzzle_input)
+solution1, solution2 = solve(puzzle_data)
+
+print(solution1)
+print(solution2)
