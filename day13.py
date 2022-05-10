e31c87f10e4ec0e3f7ca20971370152390792bfb
mjenks <rikku.hotaru@gmail.com>
2022-05-09 22:29:24 -0400

    part 1
    added functions for handling movement and intersections to the cart
    class. Moved carts until a collision was found

--- day13.py
+++ day13.py
@@ -5,13 +5,33 @@ Created on Mon May 09 13:37:35 2022
 @author: mjenks
 """
 
+import collections
+
 class Cart():
     turns = ['l', 's', 'r']
+#    compass = ['u', 'r', 'd', 'l']
     def __init__(self, x, y, facing):
         self.turn_index = 0
         self.x = x
         self.y = y
         self.facing = facing
+    def move(self):
+        if self.facing == 3:
+            self.x -= 1
+        elif self.facing == 0:
+            self.y -= 1
+        elif self.facing == 1:
+            self.x += 1
+        elif self.facing == 2:
+            self.y += 1
+    def turn(self):
+        trn = self.turns[self.turn_index]
+        self.turn_index = (self.turn_index + 1)%3
+        if trn == 'l':
+            self.facing -= 1
+        elif trn == 'r':
+            self.facing += 1
+        self.facing = self.facing%4
 
 def parse(puzzle_input):
     data = []
@@ -22,23 +42,45 @@ def parse(puzzle_input):
     for j in range(len(data)):
         for i in range(len(data[0])):
             if data[j][i] == '<':
-                carts.append(Cart(i, j, 'l'))
+                carts.append(Cart(i, j, 3))
                 data[j][i] = '-'
             elif data[j][i] == '>':
-                carts.append(Cart(i, j, 'r'))
+                carts.append(Cart(i, j, 1))
                 data[j][i] = '-'
             elif data[j][i] == 'v':
-                carts.append(Cart(i, j, 'd'))
+                carts.append(Cart(i, j, 2))
                 data[j][i] = '|'
             elif data[j][i] == '^':
-                carts.append(Cart(i, j, 'u'))
+                carts.append(Cart(i, j, 0))
                 data[j][i] = '|'
                 
     return data, carts
     
 def solve(puzzle_data):
     track, carts = puzzle_data
-    return 0, 0
+    ticks = 0
+    collision = False
+    while not collision:
+        for cart in carts:
+            cart.move()
+            if track[cart.y][cart.x] == '/':
+                if cart.facing%2 == 0:
+                    cart.facing = (cart.facing + 1)%4
+                else:
+                    cart.facing = (cart.facing - 1)%4
+            if track[cart.y][cart.x] == '\\':
+                if cart.facing%2 == 0:
+                    cart.facing = (cart.facing - 1)%4
+                else:
+                    cart.facing = (cart.facing + 1)%4
+            elif track[cart.y][cart.x] == '+':
+                cart.turn()
+        ticks += 1
+        locations = [(c.x, c.y) for c in carts]
+        if len(locations) != len(set(locations)):
+            collision = True
+    collision_points = [a for a, b in collections.Counter(locations).items() if b > 1]
+    return collision_points, 0
 
 puzzle_path = "input_day13.txt"
 with open(puzzle_path) as f:
