3642ec5502a9fbe5e14ea27c7cf128042c2ab9bb
mjenks <rikku.hotaru@gmail.com>
2022-05-12 11:47:02 -0400

    part 1
    created recipies until 10 more than input

--- day14.py
+++ day14.py
@@ -4,19 +4,21 @@ Created on Thu May 12 11:26:48 2022
 
 @author: mjenks
 """
-
-def parse(puzzle_input):
-    data = []
-    return data
     
 def solve(puzzle_data):
-    return 0, 0
+    recipes = [3,7]
+    elf1 = 0
+    elf2 = 1
+    while len(recipes) < 10 + puzzle_data:
+        new = recipes[elf1] + recipes[elf2]
+        for digit in list(str(new)):
+            recipes.append(int(digit))
+        elf1 = (elf1 + recipes[elf1] + 1)%len(recipes)
+        elf2 = (elf2 + recipes[elf2] + 1)%len(recipes)
+    return ''.join([str(x) for x in recipes[puzzle_data:puzzle_data+10]]), 0
 
-puzzle_path = "input_day14.txt"
-with open(puzzle_path) as f:
-    puzzle_input = f.readlines()
-    
-puzzle_data = parse(puzzle_input)
+
+puzzle_data = 540391
 solution1, solution2 = solve(puzzle_data)
 
 print(solution1)
