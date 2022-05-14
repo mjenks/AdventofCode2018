28c36d3393772cf2cbb0cb4fa111beeeee3da6dc
mjenks <rikku.hotaru@gmail.com>
2022-05-13 21:52:44 -0400

    part 1
    simulates the battle successfully for all examples, but doesn't give the
    solution with the input. I am not sure why

--- day15.py
+++ day15.py
@@ -6,7 +6,8 @@ Created on Fri May 13 10:41:20 2022
 """
 
 class Unit:
-    def __init__(self, x, y):
+    def __init__(self, race, x, y):
+        self.race = race
         self.hp = 200
         self.attack = 3
         self.x = x
@@ -16,26 +17,180 @@ def parse(puzzle_input):
     data = []
     for line in puzzle_input:
         data.append(list(line.strip()))
-    goblins = []
-    elves = []
+    units = []
     for j in range(len(data)):
         for i in range(len(data[0])):
             if data[j][i] == 'G':
-                goblins.append(Unit(i,j))
+                units.append(Unit('G',i,j))
             elif data[j][i] == 'E':
-                elves.append(Unit(i,j))
-    return data, goblins, elves
+                units.append(Unit('E',i,j))
+    return data, units
+    
+def inrange(enemies):
+    global grid
+    avail = []
+    for enemy in enemies:
+        i = enemy.x
+        j = enemy.y
+        if i > 0:
+            point = grid[j][i-1]
+            if point == '.':
+                avail.append((i-1,j))
+        if i < len(grid[0]):
+            point = grid[j][i+1]
+            if point == '.':
+                avail.append((i+1,j))
+        if j > 0:
+            point = grid[j-1][i]
+            if point == '.':
+                avail.append((i,j-1))
+        if j < len(grid):
+            point = grid[j+1][i]
+            if point == '.':
+                avail.append((i,j+1))
+    return avail
+    
+def isopen(x,y):
+    global grid
+    if grid[y][x] == '.':
+        return True
+    else:
+        return False
+    
+def step(current, visited):
+    options = []
+    for point in current:
+        x, y = point
+        if isopen(x+1,y) and (x+1,y) not in visited:
+            spot = x+1, y
+            visited.add(spot)
+            options.append(spot)
+        if isopen(x,y+1) and (x,y+1) not in visited:
+            spot = x, y+1
+            visited.add(spot)
+            options.append(spot)
+        if isopen(x-1, y) and (x-1,y) not in visited:
+            spot = x-1, y
+            visited.add(spot)
+            options.append(spot)
+        if isopen(x, y-1) and (x,y-1) not in visited:
+            spot = x, y-1
+            visited.add(spot)
+            options.append(spot)
+    return options, visited    
+    
+def findNearest(unit, points):
+    location = [(unit.x,unit.y)]
+    steps = 0
+    shortest_routes = [0 for a in range(len(points))]
+    visited = set()
+    visited.add(location[0])
+    nearest = []
+    while sum(shortest_routes) == 0 and len(location) > 0 and steps < 1000:
+        steps += 1
+        location, visited = step(location, visited)
+        for spot in location:
+            if spot in points:
+                index = points.index(spot)
+                if shortest_routes[index] == 0:
+                    shortest_routes[index] = steps
+                    nearest.append(spot)
+    if len(nearest) == 0:
+        return (unit.x, unit.y)
+    else:
+        nearest.sort(key=lambda p: (p[1], p[0]))
+        return nearest[0]
+    
+def findPath(unit, des):
+    location = [des]
+    steps = 0
+    visited = set()
+    visited.add(location[0])
+    points = [(unit.x,unit.y-1),(unit.x-1,unit.y),(unit.x+1,unit.y),(unit.x,unit.y+1)]
+    found = False
+    while not found:
+        steps += 1
+        for spot in location:
+            if spot in points:
+                choice = spot 
+                found = True
+                break
+        location, visited = step(location, visited)
+    return choice
+    
+def move(unit, enemies):
+    global grid
+    enemies = [e for e in enemies if e.hp > 0]
+    e_loc = set([(e.x,e.y) for e in enemies])
+    adj = set([(unit.x+1,unit.y),(unit.x-1,unit.y),(unit.x,unit.y+1),(unit.x,unit.y-1)])
+    if len(e_loc & adj) != 0:
+        return
+    options = inrange(enemies)
+    nearest = findNearest(unit,options)
+    if nearest == (unit.x,unit.y):
+        return
+    new_x, new_y = findPath(unit,nearest)
+    grid[unit.y][unit.x] = '.'
+    grid[new_y][new_x] = unit.race
+    unit.x = new_x
+    unit.y = new_y
+    return
+    
+def attack(unit,enemies):
+    global grid
+    inrange = []
+    for e in [i for i in enemies if i.hp > 0]:
+        if e.y == unit.y:
+            if e.x == unit.x + 1 or e.x == unit.x - 1:
+                inrange.append(e)
+        elif e.x == unit.x:
+            if e.y == unit.y + 1 or e.y == unit.y - 1:
+                inrange.append(e)
+    if len(inrange) == 0:
+        return
+    inrange.sort(key=lambda e: (e.hp, e.y, e.x))
+    target = inrange[0]
+    target.hp -= unit.attack
+    if target.hp <= 0:
+        grid[target.y][target.x] = '.'
+    return
     
 def solve(puzzle_data):
-    grid, goblins, elves = puzzle_data
-
-    return 0, 0
+    global grid
+    all_units = puzzle_data
+    goblins = [x for x in all_units if x.race == 'G']
+    elves = [x for x in all_units if x.race == 'E']
+    combat = True
+    rounds = 0
+    while combat:
+        for unit in all_units:
+            if unit.hp <= 0:
+                continue
+            if unit.race == 'G':
+                target = elves
+            elif unit.race == 'E':
+                target = goblins
+            if len([t for t in target if t.hp > 0]) == 0:
+                combat = False
+                break
+#            print unit.race, unit.y, unit.x
+            move(unit,target)
+            attack(unit,target)
+        all_units.sort(key=lambda u: (u.y, u.x))
+        if combat:
+            rounds += 1
+#            print rounds
+#        for row in grid:
+#            print ''.join(row)
+    winners_hp = sum([u.hp for u in all_units if u.hp > 0])
+    return rounds*winners_hp, 0
 
 puzzle_path = "input_day15.txt"
+#puzzle_path = "day15_ex6.txt"
 with open(puzzle_path) as f:
     puzzle_input = f.readlines()
     
-puzzle_data = parse(puzzle_input)
+grid, puzzle_data = parse(puzzle_input)
 solution1, solution2 = solve(puzzle_data)
 
 print(solution1)
