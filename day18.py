0d0497dc784578951f41ad2ee4eccb0c502fa1e7
mjenks <rikku.hotaru@gmail.com>
2022-06-05 13:14:43 -0400

    part 1
    read in input and code to morph the grid over each minute

--- day18.py
+++ day18.py
@@ -7,10 +7,61 @@ Created on Sun Jun 05 12:13:14 2022
 
 def parse(puzzle_input):
     data = []
+    for line in puzzle_input:
+        line = line.strip()
+        data.append(list(line))
     return data
+
+def morph(grid):
+    new_grid = []
+    for j in range(50):
+        new_row = []
+        for i in range(50):
+            adjecent = []
+            if j != 0:
+                if i != 0:
+                    adjecent.append(grid[j-1][i-1])
+                adjecent.append(grid[j-1][i])
+                if i != 49:
+                    adjecent.append(grid[j-1][i+1])
+            if j != 49:
+                if i != 0:
+                    adjecent.append(grid[j+1][i-1])
+                adjecent.append(grid[j+1][i])
+                if i != 49:
+                    adjecent.append(grid[j+1][i+1])
+            if i != 0:
+                adjecent.append(grid[j][i-1])
+            if i != 49:
+                adjecent.append(grid[j][i+1])
+            cur = grid[j][i]
+            if cur == '.':
+                if sum([1 for x in adjecent if x == '|']) >= 3:
+                    new_row.append('|')
+                else:
+                    new_row.append(cur)
+            elif cur == '|':
+                if sum([1 for x in adjecent if x == '#']) >= 3:
+                    new_row.append('#')
+                else:
+                    new_row.append(cur)
+            else:
+                if '|' in adjecent and '#' in adjecent:
+                    new_row.append(cur)
+                else:
+                    new_row.append('.')
+        new_grid.append(new_row)
+    return new_grid    
     
 def solve(puzzle_data):
-    return 0, 0
+    grid = puzzle_data
+    time = 0
+    while time < 10:
+        grid = morph(grid)
+        time += 1
+    wooded = sum([sum([1 for x in row if x == '|']) for row in grid])
+    lumber = sum([sum([1 for x in row if x == '#']) for row in grid])
+    return wooded*lumber, 0
 
 puzzle_path = "input_day18.txt"
 with open(puzzle_path) as f:
